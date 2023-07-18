1a612d9034641cfbe07bde78efa3d59db68191eb
Patrickhell <patrickhell19@yahoo.fr>
2023-07-18 19:19:47 +0200

    first commit

---
+++ resources/views/header.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <div>
+            <h4>
+                <a href=" {{route('Help')}}">
+                    Home
+                </a>
+            </h4>
+            <h4>
+                <a href=" {{route('About-Us')}}">
+                    About Us
+                </a>
+            </h4>
+            <h4>
+                <a href=" {{route('Home)}}">
+                    Help
+                </a>
+            </h4>
+        </div>
+
+    </header>
+</body>
+
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primi passi Php</title>
+</head>
+
+<body>
+
+    <main>
+        <h1>
+            Hello Word
+        </h1>
+        <span>
+            {{$name}},
+        </span>
+        <span>
+            {{$age}},
+        </span>
+        <span>
+            {{$date_of_birth}},
+        </span>
+    </main>
+
+
+</body>
+
+</html>
